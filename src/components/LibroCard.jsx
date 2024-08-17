fdf82d53f1e5a758802ad9cc0495ee1ebc2a10f2
mbrionesz <matias.briones95@gmail.com>
2024-08-16 22:23:15 -0400

    Imagenes y modificación en json, librocard

--- src/components/LibroCard.jsx
+++ src/components/LibroCard.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 function LibroCard({ libro }) {
   return (
     <div className="card">
-      <img src={libro.imagen} alt={libro.nombre} className="card-img-top" />
+      <img src={libro.img} alt={libro.titulo} className="card-img-top" />
       <div className="card-body">
-        <h5 className="card-title">{libro.nombre}</h5>
-        <p className="card-text">{libro.descripcion}</p>
-        <p className="card-text">{libro.precio} USD</p>
+        <h5 className="card-title">{libro.titulo}</h5>
+        <p className="card-text">{libro.desc}</p>
+        <p className="card-text">{libro.price} {libro.moneda}</p>
       </div>
     </div>
   );
